0735f40f741022a38db98af5171e28ea526696ef
hjc <hjc_0827@outlook.com>
2025-07-11 16:34:45 +0800

    feat: Add token cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,8 +9,9 @@ import { TodoItemsModule } from './todo-items/todo-items.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './common/auth-guard.guard';
+import { GlobalExceptionFilter } from './common/global-exception.filter';
 
 @Module({
   imports: [
@@ -47,6 +48,16 @@ import { AuthGuard } from './common/auth-guard.guard';
   providers: [AppService, {
     provide: APP_GUARD,
     useClass: AuthGuard
-  }],
+  }, {
+      provide: APP_FILTER,
+      useValue: new ValidationPipe({
+        transform: true,        // 自动类型转换（如字符串转数字）
+        whitelist: true,         // 过滤 DTO 未定义的属性
+        forbidNonWhitelisted: true, // 禁止额外属性，返回 400 错误
+      })
+    }, {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    }],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './common/global-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new GlobalExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,        // 自动类型转换（如字符串转数字）
-    whitelist: true,         // 过滤 DTO 未定义的属性
-    forbidNonWhitelisted: true, // 禁止额外属性，返回 400 错误
-  }))
+  // app.useGlobalFilters(new GlobalExceptionFilter());
+  // app.useGlobalPipes(new ValidationPipe({
+  //   transform: true,        // 自动类型转换（如字符串转数字）
+  //   whitelist: true,         // 过滤 DTO 未定义的属性
+  //   forbidNonWhitelisted: true, // 禁止额外属性，返回 400 错误
+  // }))
   app.enableCors({
     origin: '*', // 允许所有来源
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 允许的HTTP方法
